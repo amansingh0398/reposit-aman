feaf55ff071c7d5f6f87a2f0efd70f16547dffe4
amansingh0398 <103134021+amansingh0398@users.noreply.github.com>
2022-05-18 10:58:22 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './Components/Home'
+
+import {BrowserRouter as Router,Route,Routes,} from 'react-router-dom'
+
+function App() {
+  return (
+    <div className="App">
+     
+     <Home/>
+    
+        
+    </div>
+  );
+}
+
+export default App;
